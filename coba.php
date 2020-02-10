2d3998d17bb8a2bb4142cb9ac86fe9939f59df49
septian <septianadhiwicak@gmail.com>
2020-02-10 10:42:55 +0700

    baru di push

---
+++ coba.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- PAGE settings -->
+  <!-- CSS dependencies -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/stcolor.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="assets/css/docs.theme.min.css">
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.dataTables.min.js"></script>
+  <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+  <script src='assets/js/Chart.min.js'></script>
+  <script src="assets/js/bootstrap-select.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <title>Document</title>
+
+</head>
+<body>
+  <div class="owl-carousel owl-theme">
+    <div class="item"><h4>1</h4></div>
+    <div class="item"><h4>2</h4></div>
+    <div class="item"><h4>3</h4></div>
+    <div class="item"><h4>4</h4></div>
+    <div class="item"><h4>5</h4></div>
+    <div class="item"><h4>6</h4></div>
+    <div class="item"><h4>7</h4></div>
+    <div class="item"><h4>8</h4></div>
+    <div class="item"><h4>9</h4></div>
+    <div class="item"><h4>10</h4></div>
+    <div class="item"><h4>11</h4></div>
+    <div class="item"><h4>12</h4></div>
+</div>
+
+  <script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/owl.carousel.js"></script>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- PAGE settings -->
+  <!-- CSS dependencies -->
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/stcolor.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="assets/css/docs.theme.min.css">
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.dataTables.min.js"></script>
+  <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+  <script src='assets/js/Chart.min.js'></script>
+  <script src="assets/js/bootstrap-select.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+
+ <title> Kopi Ku </title>
+</head>
+<body>
+
+<div class="justify-content-center nav">
+<img class="pas1" src="assets/image/ada.png" width="80px" height="80x" title="my Kopi">
+<h1 class="font1"> My Coffe</h1>  
+</div>
+
+
+<div class="navbar navbar-expand-lg">
+    <div class="collapse navbar-collapse font2" id="navbarSupportedContent">
+ 
+      <ul class="navbar-nav mr-auto">
+        <li  class="nav-item active">
+          <a class="font2" href="#">Iced Coffee </a>
+        </li>
+        <li class="nav-item active">
+          <a class="font2" href="#">Cafe Mocha</a>
+        </li>
+        <li class="nav-item">
+          <a class="font2" href="#">Cappucino</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="font2 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Tutorial
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Tutorial Barista</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Tutorial Pelayan</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Tutorial Management</a>
+          </div>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="font2 disabled" href="#" tabindex="-1" aria-disabled="true">Link Mati</a>
+        </li> -->
+        <div class="!important">
+          <a href="tokopedia.com">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </a>
+          </div>
+      </ul>
+
+ <div class="demo-2 search">
+   <form>
+   <span class="icon"><i class="fa fa-search"></i></span>
+   <input type="search" id="search" placeholder="Search..." />
+   </form>
+      <!-- <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Cari" aria-label="Cari">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
+      </form> -->
+ </div>
+   </div>
+
+ 
+</div> 
+
+ 
+  <section>
+    <div class="latar">
+
+   <!-- MAIN-CATEGORIES-AREA -->
+    <section class="maincategories-area section-padding">
+        <div class="maincategories">
+            <div class="categories-big float-left">
+                <div class="categories-img">
+                    <div class="cc_banner"><img src="assets/image/featured1.png" alt="#"></div>
+                    <div class="cat-text">
+                        <h3>Arabika</h3>
+                        <P> di buat di arab</P>
+                    </div>
+                    <div class="cat-hover">
+                        <div class="cat-in"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="categories-middle cat-mid float-left">
+                <div class="categories-up-main">
+                    <div class="categories-up">
+                        <div class="cc_banner"><img src="assets/image/featured2.png" alt="#"></div>
+                        <div class="up-text">
+                            <h3>Frutie</h3>
+                            <P>rasa buah buahan</P>
+                        </div>
+                        <div class="up-hover">
+                            <div class="up-in"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="categories-down-main">
+                    <div class="categories-down">
+                        <div class="cc_banner"><img src="assets/image/featured3.png" alt="#"></div>
+                        <div class="down-text">
+                            <h3>Americano</h3>
+                            <P>dari amerika</P>
+                        </div>
+                        <div class="down-hover">
+                            <div class="down-in"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="categories-big float-right">
+                <div class="categories-img">
+                    <div class="cc_banner"><img src="assets/image/featured4.png" alt="#"></div>
+                    <div class="cat-text right-tx">
+                        <h3>kopi enak enak</h3>
+                        <P> Kopi mantap</P>
+                    </div>
+                    <div class="cat-hover">
+                        <div class="cat-in"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- MAIN-CATEGORIES-AREA END -->
+  </div>
+</section>
+
+
+ <!-- HOT-DEAL-AREA -->
+   <section class="hot-deal-area section-padding">
+        <div class="container">
+      <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="headline">
+          <h2>Hot deal</h2>
+        </div>
+        <div class="hot-deal">
+          <div class="hot-single">
+            <a href="#"><img src="assets/image/hot_1.png" alt="">
+            </a>
+            <div class="hot-count">
+              <ul class="hc-upper">
+                <li class="counter">128</li>
+                <li class="counter">23</li>
+                <li class="counter">43</li>
+                <li class="counter">21</li>
+              </ul>
+              <ul class="hc-lower">
+                <li>Days</li>
+                <li>Hrs</li>
+                <li>Mins</li>
+                <li>Secs</li>
+              </ul>
+            </div>
+            <div class="wid-rating">
+              <h4><a href="single-product.html">Best Handbags For girls</a></h4>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+              <div class="product-wid-price">
+                <ins>$260.00</ins> <del>$280.00</del>
+              </div>
+            </div>
+          </div>
+          <div class="hot-single">
+            <a href="#"><img src="assets/image/hot_2.png" alt="">
+            </a>
+            <div class="hot-count">
+              <ul class="hc-upper">
+                <li class="counter">153</li>
+                <li class="counter">18</li>
+                <li class="counter">11</li>
+                <li class="counter">40</li>
+              </ul>
+              <ul class="hc-lower">
+                <li>Days</li>
+                <li>Hrs</li>
+                <li>Mins</li>
+                <li>Secs</li>
+              </ul>
+            </div>
+            <div class="wid-rating">
+              <h4><a href="single-product.html">stylish dress for women</a></h4>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+              <div class="product-wid-price">
+                <ins>$3000.00</ins>
+              </div>
+            </div>
+          </div>
+          <div class="hot-single">
+            <a href="#"><img src="assets/image/hot_3.png" alt="">
+            </a>
+            <div class="hot-count">
+              <ul class="hc-upper">
+                <li class="counter">156</li>
+                <li class="counter">11</li>
+                <li class="counter">20</li>
+                <li class="counter">31</li>
+              </ul>
+              <ul class="hc-lower">
+                <li>Days</li>
+                <li>Hrs</li>
+                <li>Mins</li>
+                <li>Secs</li>
+              </ul>
+            </div>
+            <div class="wid-rating">
+              <h4><a href="single-product.html">Leather Handbags For girls</a></h4>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+              <div class="product-wid-price">
+                <ins>$180.00</ins> <del>$200.00</del>
+              </div>
+            </div>
+          </div>
+          <div class="hot-single">
+            <a href="#"><img src="assets/image/hot_4.png" alt="">
+            </a>
+            <div class="hot-count">
+              <ul class="hc-upper">
+                <li class="counter">162</li>
+                <li class="counter">08</li>
+                <li class="counter">53</li>
+                <li class="counter">05</li>
+              </ul>
+              <ul class="hc-lower">
+                <li>Days</li>
+                <li>Hrs</li>
+                <li>Mins</li>
+                <li>Secs</li>
+              </ul>
+            </div>
+            <div class="wid-rating">
+              <h4><a href="single-product.html">Trendy Shoes For Women</a></h4>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star"></i>
+              <i class="fa fa-star-half-o"></i>
+              <div class="product-wid-price">
+                <ins>$250.00</ins>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+        </div>
+    </section>
+    <!-- HOT-DEAL-AREA END -->
+     
+
+
+</body>
+</html>
